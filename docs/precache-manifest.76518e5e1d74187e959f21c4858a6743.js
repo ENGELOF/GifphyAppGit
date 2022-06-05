2c795b2d23d1df77ee58a21a53ac1cacc1d9f946
Andres Velasco <engelof@hotmail.com>
2022-06-05 15:44:13 -0500

    docs agregados

---
+++ docs/precache-manifest.76518e5e1d74187e959f21c4858a6743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a29487d2456db436afd839391147025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9368feccfd58f8dba975",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9368feccfd58f8dba975",
+    "url": "/static/js/main.1dccdcc5.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
